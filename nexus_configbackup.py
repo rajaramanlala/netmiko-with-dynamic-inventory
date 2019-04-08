eeebd6ad01db98c8c3db3f3769cd43c15b9d4e15
rajaramanlala <shenbagamoorthyrajaraman@gmail.com>
2019-04-08 18:56:14 +0530

    Nexus config back up script
    Can take backup of multiple nexus devices. Device list is pulled from inventory.yml file.

---
+++ nexus_configbackup.py
@@ -0,0 +1,54 @@
+import netmiko
+from myhelper import read_yaml, form_connection_params_from_yaml
+import logging
+
+#logging.basicConfig(filename='dynamic.log', level=logging.DEBUG)
+#logger = logging.getLogger("netmiko")
+
+
+COMMANDS_LIST = [
+         "show configuration | no-more"
+])
+
+
+def collect_outputs(devices, commands):
+    """
+    Collects commands from the dictionary of devices
+    Args:
+        devices (dict): dictionary, where key is the hostname, value is
+            netmiko connection dictionary
+        commands (list): list of commands to be executed on every device
+    Returns:
+        dict: key is the hostname, value is string with all outputs
+    """
+    for device in devices:
+        hostname = device.pop("hostname")
+        connection = netmiko.ConnectHandler(**device)
+        print(hostname)
+
+        for command in commands:
+            print( command)
+            command_result = connection.send_command(command, delay_factor=10)
+            with open(hostname , "a") as f:
+                 f.write(command)
+                 f.write(command_result)
+            connection.disconnect()
+            yield command_result
+
+def getdevicename(parsed_yaml):
+     parsed_yaml = read_yaml()
+     for site_dict in parsed_yaml["all"]["sites"]:
+         for host in site_dict["hosts"]:
+              device_name = host.get('hostname')
+              yield device_name
+
+
+def main():
+    parsed_yaml = read_yaml()
+    connection_params = form_connection_params_from_yaml(parsed_yaml)
+    for device_output in collect_outputs(connection_params, COMMANDS_LIST):
+          print(device_output)
+              
+
+if __name__ == "__main__":
+    main()
